2739e08241fd3786c47087029ee62d1f6615e019
xzcxvc <daryl.tiamps@gmail.com>
2025-06-06 23:32:17 +0800

    add placeholder image

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-
+import dariru from "../../assets/dariru.png";
 const Home = () => {
   return (
     <>
       {/* Home Section */}
       <section
         id="home"
-        className="min-h-screen flex items-center justify-center px-4"
+        className="min-h-screen flex lg:flex-row flex-col items-center justify-center px-4 gap-12"
       >
-        <div className="text-center">
+        <div className="text-center order-2 lg;order-1">
           <h1 className="text-5xl md:text-7xl font-bold text-gray-900 dark:text-white mb-6">
-            Welcome to <span className="text-green-500">Dariru Codes</span>
+            Welcome to <br />
+            <span className="text-green-500">
+              {"<"}Dariru Codes{" />"}
+            </span>
           </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-300 max-w-2xl mx-auto">
+          <p className="text-xl  text-gray-600 dark:text-gray-300 max-w-2xl mx-auto">
             Full-stack developer creating amazing web experiences with modern
             technologies.
           </p>
+        </div>
+
+        <div className="w-full lg:order-2 order-1 lg:max-w-lg max-w-sm aspect-square overflow-hidden rounded-full">
+          <img
+            src={dariru}
+            alt="Profile Picture"
+            className="w-full object-cover"
+          />
         </div>
       </section>
     </>
